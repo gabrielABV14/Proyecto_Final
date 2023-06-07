2012198c4d9e0320f801c09c7b5ef67794ae29eb
gabrielABV14 <129082013+gabrielABV14@users.noreply.github.com>
2023-06-07 00:44:53 -0400

    Add files via upload
    codigo 2, con sensor MQ-8

---
+++ VersionesDelCodigo/Arduino/Esclavo_i2c_prueba_2.ino
@@ -0,0 +1,48 @@
+// incluir libreria
+#include <Wire.h>
+// definir valores 
+int p = 0; // sino
+int q = 0; // valor1 
+int r = 0; // valor2
+int d = 0;
+void setup() {
+// configurar direcion i2c
+Wire.begin(0x30); 
+  // declarar evento de recibir 
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(Evento);
+  Serial.begin(9600);
+}
+ // definimos funcion de recibimiento
+void receiveEvent(int howMany) {
+  while (Wire.available()) { 
+     // tomamos el registro de dato de i2c
+     p = Wire.read(); 
+     // tomamos el primer dato de i2c
+     q = Wire.read();
+     // tomamos el segundo dato de i2c
+     r = Wire.read();
+    // devolvemos el valor de su formato de bytes a entero 
+    int dato = (q*256)+r;
+    if(p == 0){
+      dato = dato*(-1);
+    }
+    if(dato == 890)
+    {
+      Serial.print("Se envio la orden al sensor:  ");
+      d = analogRead(A0);
+      Serial.println(d);
+    }
+    Serial.println(dato);
+    Evento();
+  }
+}
+void loop() {
+  //delay(10);
+  // no viene nada temporalmente 
+}
+
+void Evento()
+{
+  Wire.write(d);
+}
