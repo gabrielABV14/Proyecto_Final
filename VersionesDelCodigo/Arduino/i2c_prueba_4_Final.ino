9eac4994a86bd5900cdc784778a1f3980675e91c
gabrielABV14 <129082013+gabrielABV14@users.noreply.github.com>
2023-06-08 23:15:16 -0400

    Versión final del código.

---
+++ VersionesDelCodigo/Arduino/i2c_prueba_4_Final.ino
@@ -0,0 +1,63 @@
+// incluir libreria
+#include <Wire.h>
+// definir valores 
+int p = 0; // sino
+int q = 0; // valor1 
+int r = 0; // valor2
+int d = 0;
+int pin= 13;
+int pin2= 12;
+void setup() {
+// configurar direcion i2c
+Wire.begin(0x30); 
+  // declarar evento de recibir 
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(Evento);
+  Serial.begin(9600);
+  pinMode(pin,OUTPUT);
+  digitalWrite(pin,HIGH);
+}
+ // definimos funcion de recibimiento
+void receiveEvent(int howMany) {
+  while (Wire.available()) { 
+     // tomamos el registro de dato de i2c
+     p = Wire.read(); 
+     // tomamos el primer dato de i2c
+     q = Wire.read();
+     // tomamos el segundo dato de i2c
+     r = Wire.read();
+    // devolvemos el valor de su formato de bytes a entero 
+    int dato = (q*256)+r;
+    if(p == 0){
+      dato = dato*(-1);
+    }
+    if(dato == 1)
+    {
+      digitalWrite(pin,LOW);
+      digitalWrite(pin2,LOW);
+    }
+    if(dato == 0)
+    {
+      digitalWrite(pin,HIGH);
+      digitalWrite(pin2,LOW);
+    }
+    if(dato == 890)
+    {
+      Serial.print("Se envio la orden al sensor:  ");
+      d = analogRead(A0);
+      Serial.println(d);
+      
+    }
+    Serial.println(dato);
+    Evento();
+  }
+}
+void loop() {
+  //delay(10);
+  // no viene nada temporalmente 
+}
+
+void Evento()
+{
+  Wire.write(d);
+}
